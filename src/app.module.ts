cdb6bc3f8ff1ff4108ea4a867ebf5a26b5a7ee6e
Diego Vicuna <diego.vicuna@funiber.org>
2023-10-31 11:28:35 -0500

    feature: configurable modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,22 @@ import { CoffeesModule } from './coffees/coffees.module';
 import {SchedulerModule} from './scheduler/scheduler.module'
 import { CronModule } from './cron/cron.module';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
+import { HttpClientModule } from './http-client/http-client.module';
 @Module({
-  imports: [CoffeesModule, SchedulerModule, CronModule, FibonacciModule],
+  imports: [
+    CoffeesModule,
+    SchedulerModule,
+    CronModule,
+    FibonacciModule,
+    HttpClientModule.register({
+      baseUrl: 'http://nestjs.com',
+      isGlobal:true
+    })
+    // Alternatively:
+    // HttpClientModule.registerAsync({
+    //   useFactory: () => ({ baseUrl: 'http://nestjs.com' }),
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
